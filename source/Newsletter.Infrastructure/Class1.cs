0bd01474dcdf4b4b4b5641a05c98c639b8585913
Thalles Teodoro <thallesmjteodoro@outlook.com>
2023-06-23 13:35:59 -0300

    chore: 🐳 project setup

---
+++ source/Newsletter.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Newsletter.Infrastructure;
+
+public class Class1
+{
+
+}
---
+++ tests/Newsletter.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Newsletter.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
