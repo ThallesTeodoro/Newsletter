b42ad984f160001516bdb83838f1a306914134bc
Thalles Teodoro <thallesmjteodoro@outlook.com>
2023-06-23 15:43:59 -0300

    🐞 fix: editor config IDE0073 severity to none

--- source/Newsletter.Api/Program.cs
+++ source/Newsletter.Api/Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddCarter();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-// if (app.Environment.IsDevelopment())
-// {
-// }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
